ec2e81c94fcbfcacdfda95e6be40690a609f1b04
0x53616d <47088044+0x53616d@users.noreply.github.com>
2021-09-08 20:48:45 -0700

    Initial project upload

---
+++ Collatz Conjecture POC/Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.NumberToStartAt = New System.Windows.Forms.TextBox()
+        Me.StartButton = New System.Windows.Forms.Button()
+        Me.StopButton = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CollatzLog = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Starting Number"
+        '
+        'NumberToStartAt
+        '
+        Me.NumberToStartAt.Location = New System.Drawing.Point(3, 16)
+        Me.NumberToStartAt.Name = "NumberToStartAt"
+        Me.NumberToStartAt.Size = New System.Drawing.Size(114, 20)
+        Me.NumberToStartAt.TabIndex = 1
+        Me.NumberToStartAt.Text = "1"
+        '
+        'StartButton
+        '
+        Me.StartButton.Location = New System.Drawing.Point(3, 42)
+        Me.StartButton.Name = "StartButton"
+        Me.StartButton.Size = New System.Drawing.Size(114, 23)
+        Me.StartButton.TabIndex = 2
+        Me.StartButton.Text = "Begin"
+        Me.StartButton.UseVisualStyleBackColor = True
+        '
+        'StopButton
+        '
+        Me.StopButton.Location = New System.Drawing.Point(3, 71)
+        Me.StopButton.Name = "StopButton"
+        Me.StopButton.Size = New System.Drawing.Size(114, 23)
+        Me.StopButton.TabIndex = 3
+        Me.StopButton.Text = "Stop"
+        Me.StopButton.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(3, 100)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(91, 17)
+        Me.CheckBox1.TabIndex = 4
+        Me.CheckBox1.Text = "Complete Log"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CollatzLog
+        '
+        Me.CollatzLog.Location = New System.Drawing.Point(123, 0)
+        Me.CollatzLog.Name = "CollatzLog"
+        Me.CollatzLog.Size = New System.Drawing.Size(166, 117)
+        Me.CollatzLog.TabIndex = 5
+        Me.CollatzLog.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(290, 119)
+        Me.Controls.Add(Me.CollatzLog)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.StopButton)
+        Me.Controls.Add(Me.StartButton)
+        Me.Controls.Add(Me.NumberToStartAt)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Collatz Conjecture POC"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents NumberToStartAt As TextBox
+    Friend WithEvents StartButton As Button
+    Friend WithEvents StopButton As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CollatzLog As RichTextBox
+End Class
---
+++ Collatz Conjecture POC/Form1.vb
@@ -0,0 +1,82 @@
+﻿Public Class Form1
+    Shared Mathing As Boolean
+    Shared numbertodo As Int64
+    Shared numbertodoorig As Int64
+    Shared highestnum As Int64
+    Shared mathcount As Int64
+    Public Async Sub DoMathBadly()
+        'Convert string of number to integer
+        numbertodo = NumberToStartAt.Text
+        numbertodoorig = numbertodo
+        'Overwrite last highest number
+        highestnum = numbertodo
+        Do While Mathing = True
+            'Check for new highest number
+            If numbertodo > highestnum Then
+                highestnum = numbertodo
+            End If
+
+            'Do collatz math
+            If IsOdd(numbertodo) Then
+                numbertodo = ((numbertodo * 3) + 1)
+                mathcount = mathcount + 1
+            ElseIf IsOdd(numbertodo) = False Then
+                numbertodo = numbertodo / 2
+                mathcount = mathcount + 1
+            End If
+
+            'Log work
+            If CheckBox1.Checked = True Then
+                My.Computer.FileSystem.WriteAllText("E:\Dextop\stupid\Collatz Conjecture Rough Log.txt", numbertodo.ToString + Environment.NewLine, True)
+            End If
+            'Stop at loop numbers
+            If numbertodo = 1 Then
+                'Log
+                If CollatzLog.Text = "" Then
+                    CollatzLog.AppendText(numbertodoorig.ToString + ": " + mathcount.ToString + ", " + highestnum.ToString)
+                Else
+                    CollatzLog.AppendText(Environment.NewLine + numbertodoorig.ToString + ": " + mathcount.ToString + ", " + highestnum.ToString)
+                    CollatzLog.SelectionStart = CollatzLog.Text.Length
+                    CollatzLog.ScrollToCaret()
+                End If
+                My.Computer.FileSystem.WriteAllText("E:\Dextop\stupid\Collatz Conjecture Log.txt", numbertodoorig.ToString + ": " + mathcount.ToString + ", " + highestnum.ToString + Environment.NewLine, True)
+                'Restart with new number
+                NumberToStartAt.Text = numbertodoorig + 1
+                Await Task.Delay(5)
+                'Convert string of number to integer
+                numbertodo = NumberToStartAt.Text
+                numbertodoorig = numbertodo
+                'Overwrite last highest number
+                highestnum = numbertodo
+                'Math operation count resetter
+                mathcount = 0
+            End If
+        Loop
+    End Sub
+
+    Function IsOdd(number As Integer) As Boolean
+        Dim tNum As Integer
+        tNum = number
+        Dim tNumDiv2 = tNum / 2
+        If tNumDiv2.ToString.Contains(".") Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Private Sub StartButton_Click(sender As Object, e As EventArgs) Handles StartButton.Click
+        Mathing = True
+        If My.Computer.FileSystem.FileExists("E:\Dextop\stupid\Collatz Conjecture Log.txt") And My.Computer.FileSystem.FileExists("E:\Dextop\stupid\Collatz Conjecture Rough Log.txt") Then
+            My.Computer.FileSystem.DeleteFile("E:\Dextop\stupid\Collatz Conjecture Log.txt")
+            If CheckBox1.Checked = True Then
+                My.Computer.FileSystem.DeleteFile("E:\Dextop\stupid\Collatz Conjecture Rough Log.txt")
+            End If
+        End If
+        DoMathBadly()
+    End Sub
+    Private Sub StopButton_Click(sender As Object, e As EventArgs) Handles StopButton.Click
+        Mathing = False
+    End Sub
+
+End Class
