7664b2168c96ac9986404e80d13b9dc09b4fb0cd
0x53616d <47088044+0x53616d@users.noreply.github.com>
2021-09-15 20:07:52 -0700

    Elaborated on comments
    wanted to make this repo mainly python lol

--- Collatz Conjecture Python POC/Collatz_Conjecture_Python_POC.py
+++ Collatz Conjecture Python POC/Collatz_Conjecture_Python_POC.py
@@ -1,4 +1,4 @@
-# Import sleeping, ended up not using this because any sleeps harshly limits potential.
+# Import sleeping, ended up not using this anyway, because any sleeps harshly limit potential.
 from time import sleep
 
 
@@ -9,6 +9,7 @@ global highestnum
 global mathcount
 global highestnumsofar
 global record
+# May implement threading later down the line but just seems unnecessary for now. This app isn't really maaking any progress on the issue anyway.
 
 def math(numtostart):
 
@@ -20,7 +21,7 @@ def math(numtostart):
     highestnumsofar = 0
     record = 0
 
-    # Make new line in console to look nice (\n was making 2 lines).
+    # Make new line in console to look nice (\n was making 2 lines, this worked fine so whatever lol a long as it works I guess).
     print("")
 
     # Use w (write) mode to overwrite any file thats there, or create it if it doesn't exist.
@@ -28,7 +29,7 @@ def math(numtostart):
     logfile.write("")
     logfile.close
 
-    # Avoid arithmetic operation overflow due to size exceeding max of 64 bit signed int.
+    # Avoid arithmetic operation overflow due to size exceeding max of 64 bit signed int (Again, this app isn't really making any progress on this issue, as all numbers to 2^86 have been checked against this.).
     while int(numbertodoorig) < 9223372036854775807:
      
     # Log highest numbers seen per number tried and in general.
@@ -37,7 +38,7 @@ def math(numtostart):
            highestnumsofar = numbertodo
        highestnum = numbertodo
 
-       # Checking if number is odd or even and acting accordingly.
+       # Checking if number is odd or even and acting accordingly. This is the function that handles all of the actual math operation, the conjecture implementation.
      if (int(numbertodo) % 2) == 0:  
         numbertodo = int(numbertodo) / 2
         mathcount = int(mathcount) + 1
@@ -45,7 +46,7 @@ def math(numtostart):
         numbertodo = (int(numbertodo) * 3) + 1
         mathcount = int(mathcount) + 1
         
-        # If the number hits 1, it's finished and log that to files and console output.
+        # If the number being attempted eventually falls down to 1, it's finished and the app logs that to files and console output.
      if numbertodo == 1:
         print(str(numbertodoorig) + ": " + str(mathcount) + ", " + str(highestnum) + "\n")
         logfile = open("C:\Program Files\Collatz Conjecture\Collatz Conjecture Log.txt","a")  
@@ -60,10 +61,10 @@ def math(numtostart):
             logfile = open("C:\Program Files\Collatz Conjecture\Collatz Conjecture Log.txt","a")  
             logfile.write("New record! " + str(highestnumsofar) + "\n")
             logfile.close
-            logfile = open("C:\Program Files\Collatz Conjecture\Collatz Conjecture Record Log.txt","w")  
+            logfile = open("C:\Program Files\Collatz Conjecture\Collatz Conjecture Record Log.txt","w")  # Open in w only mode so as to remove the current contents without having to delete the file every time.
             logfile.write("Current record is " + str(highestnumsofar))
             logfile.close
             record = highestnumsofar
 
-        # Fairly self explanatory, just asking where to start
+        # Fairly self explanatory, just asking where to start.
 math(input("What number should the program start at?\n"))
